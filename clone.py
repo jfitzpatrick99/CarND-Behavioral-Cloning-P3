134127fc9d73d1c6473dd95c21e70675dab1d8eb
John Fitzpatrick <jfitzpat@synopsys.com>
2017-04-08 15:35:43 -0600

    Initial network implementation.

---
+++ clone.py
@@ -0,0 +1,81 @@
+import sys
+import csv
+import cv2
+import numpy as np
+from os import path
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    print("Loading data...")
+
+    steering_correction = 0.2
+
+    images = []
+    measurements = []
+
+    with open(path.join(args[0], "driving_log.csv")) as csvfile:
+        reader = csv.reader(csvfile)
+        first_line = True
+        for line in reader:
+            if first_line:
+                first_line = False
+                continue
+
+            image = cv2.imread(path.join(args[0], line[0].strip()))
+            measurement = float(line[3])
+
+            images.append(image)
+            measurements.append(measurement)
+
+            left_image = cv2.imread(path.join(args[0], line[1].strip()))
+            images.append(left_image)
+            measurements.append(measurement + steering_correction)
+
+            right_image = cv2.imread(path.join(args[0], line[2].strip()))
+            images.append(right_image)
+            measurements.append(measurement - steering_correction)
+
+    print("Augmenting dataset...")
+    augmented_images = []
+    augmented_measurements = []
+    for image, measurement in zip(images, measurements):
+        augmented_images.append(cv2.flip(image, 1))
+        augmented_measurements.append(measurement * -1.0)
+
+    images.extend(augmented_images)
+    measurements.extend(augmented_measurements)
+
+    print("Converting data to numpy arrays...")
+    X_train = np.array(images)
+    y_train = np.array(measurements)
+
+    print("Training...")
+    model = Sequential()
+    model.add(Cropping2D(cropping=((50, 20), (0, 0)),
+                         input_shape=X_train[0].shape))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+    model.add(Conv2D(24, 5, strides=2, activation="relu"))
+    model.add(Conv2D(36, 5, strides=2, activation="relu"))
+    model.add(Conv2D(48, 5, strides=2, activation="relu"))
+    model.add(Conv2D(64, 3, strides=1, activation="relu"))
+    model.add(Conv2D(64, 3, strides=1, activation="relu"))
+
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+
+    model.compile(loss="mse", optimizer="adam")
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
+
+    model.save("model.hd5")
+    print("All done.")
+
+if __name__ == "__main__":
+    main()
