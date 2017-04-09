5f6d399827539fb81f508a9aa5afa07fdcf2a562
John Fitzpatrick <jfitzpat@synopsys.com>
2017-04-08 19:00:32 -0600

    Use keras 1.2.1 api which is what is installed on udacity AMI.

--- clone.py
+++ clone.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 from os import path
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
 
 
 def main(args=None):
@@ -59,11 +59,11 @@ def main(args=None):
     model.add(Cropping2D(cropping=((50, 20), (0, 0)),
                          input_shape=X_train[0].shape))
     model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-    model.add(Conv2D(24, 5, strides=2, activation="relu"))
-    model.add(Conv2D(36, 5, strides=2, activation="relu"))
-    model.add(Conv2D(48, 5, strides=2, activation="relu"))
-    model.add(Conv2D(64, 3, strides=1, activation="relu"))
-    model.add(Conv2D(64, 3, strides=1, activation="relu"))
+    model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
 
     model.add(Flatten())
     model.add(Dense(100))
@@ -72,7 +72,7 @@ def main(args=None):
     model.add(Dense(1))
 
     model.compile(loss="mse", optimizer="adam")
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
+    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 
     model.save("model.hd5")
     print("All done.")
